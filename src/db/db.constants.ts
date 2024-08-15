82e28680c5236078f4e46b70bdd93798a9a4593d
Ayomidedavid56 <ayomidedavid56@yahoo.com>
2024-08-15 21:09:44 +0100

    update collection settings

--- src/db/db.constants.ts
+++ src/db/db.constants.ts
@@ -1,7 +1,10 @@
 const dbConstants = {
+  // CONNECTION_STRING:
+  //   'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6',
   CONNECTION_STRING:
-    'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6',
+    'mongodb+srv://test:test@cluster85411.mvcugid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster85411',
   DATABASE_CONNECTION: 'DATABASE_CONNECTION',
+  t: '',
 };
 
 export default dbConstants;
