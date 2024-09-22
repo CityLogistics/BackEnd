bfbadf66ae0e672d837e5172b8916591e32d8816
Ayomidedavid56 <ayomidedavid56@yahoo.com>
2024-09-23 00:24:05 +0100

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,8 @@ async function bootstrap() {
       'http://admin.mycitylogistics.com',
       'http://127.0.0.1:5173',
       'http://localhost:5173',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
       'http://10.0.2.2:5173/',
       'http://127.0.0.1',
       'http://10.0.2.2',
